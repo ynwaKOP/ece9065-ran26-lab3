7b68dd3f1b7d0e3c493a388c0e186788e355aa0a
KOPRA <kopra@KOPRAs-MacBook-Pro.local>
2020-11-08 13:42:29 -0500

    implemented all event listening functions in index.js

--- static/index.js
+++ static/index.js
@@ -62,3 +62,167 @@ go2.addEventListener('click', function() {
   console.log(url);
   searchCourses(url);
 });
+
+
+const newName = document.getElementById("newName");
+const bAdd = document.getElementById("add");
+
+bAdd.addEventListener('click', function() {
+  var jsObj = { name:newName.value};
+  const url = 'http://localhost:3000/schedules';
+  console.log(url);
+  addSche(url, jsObj);
+});
+
+
+function addSche(url, jsObj) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(jsObj),
+  })
+  .then(response => response.json());
+};
+
+
+
+
+const scheName = document.getElementById("scheName");
+const bSearchSche = document.getElementById("searchSche");
+
+function searchSchedule(url) {
+  fetch(url)
+  .then(res => res.json())
+  .then(json => {
+    json.forEach(item => {
+      showResult(`${item.subject} ${item.code}`);
+    });
+  });
+}
+
+
+bSearchSche.addEventListener('click', function() {
+  var name = scheName.value;
+  const url = 'http://localhost:3000/schedules/' + name;
+  console.log(url);
+  searchSchedule(url);
+});
+
+
+
+const bALLSchedules = document.getElementById("allSchedules");
+function showSchedules(url) {
+  fetch(url)
+  .then(res => res.json())
+  .then(json => {
+    json.forEach(item => {
+      showResult(`${item.name} -- ${item.numberOfCourses} courses`);
+    });
+  });
+}
+
+
+bALLSchedules.addEventListener('click', function() {
+  const url = 'http://localhost:3000/schedulelist';
+  console.log(url);
+  showSchedules(url)
+});
+
+
+// add courses into the schedule
+const curSche = document.getElementById("curSche");
+const addSub = document.getElementById("addSubs");
+const addCode = document.getElementById("addCodes");
+const bAddCourse = document.getElementById("addCourse");
+const bReplace = document.getElementById("updateCourse");
+
+bAddCourse.addEventListener('click', function() {
+  var name = curSche.value;
+  var sub = addSub.value.split(",");
+  var cd = addCode.value.split(",");
+  var jsObj = {subject: sub,  code: cd};
+  const url = 'http://localhost:3000/schedules/' + name;
+  console.log(url);
+  addCourse(url, jsObj);
+});
+
+function addCourse(url, jsObj) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(jsObj),
+  })
+  .then(response => response.json());
+};
+
+bReplace.addEventListener('click', function() {
+  var name = curSche.value;
+  var sub = addSub.value.split(",");
+  var cd = addCode.value.split(",");
+  var jsObj = {subject: sub,  code: cd};
+  const url = 'http://localhost:3000/schedules/' + name;
+  console.log(url);
+  addCourse(url, jsObj);
+});
+
+
+function replaceCourse(url, jsObj) {
+  return fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(jsObj),
+  })
+  .then(response => response.json());
+};
+
+
+
+/*
+<label>Schedule Name</label>
+        <input id="deleteName" type="text">
+        <button id="deleteSche" type="text"> Delete THE SCHEDULE</button>
+*/
+const deleteName = document.getElementById("deleteName");
+const bDeleteSche = document.getElementById("deleteSche");
+
+function deleteSche(url) {
+  return fetch(url, {
+    method: 'DELETE',
+  })
+  .then(response => response.json());
+};
+
+bDeleteSche.addEventListener('click', function() {
+  var name = deleteName.value;
+  const url = 'http://localhost:3000/schedules/' + name;
+  console.log(url);
+  deleteSche(url);
+});
+
+
+const bdeleteAllSche = document.getElementById("deleteAllSche");
+function deleteAllSche(url) {
+  return fetch(url, {
+    method: 'DELETE',
+  })
+  .then(response => response.json());
+};
+
+bdeleteAllSche.addEventListener('click', function() {
+  const url = 'http://localhost:3000/schedulelist'
+  console.log(url);
+  deleteAllSche(url);
+});
+
+function showResult(content) {
+  const container = document.getElementById("show");
+  const element = document.createElement('p');
+  element.textContent = content;
+  container.appendChild(element);
+}
